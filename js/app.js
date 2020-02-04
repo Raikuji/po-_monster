77b6ddaedfe630ffa1e042a6b675536265d81645
lucasburte <lucas.burte@hotmail.fr>
2020-02-04 11:27:41 +0100

    add add module

---
+++ js/app.js
@@ -0,0 +1,9 @@
+import ac from 'actions.js';
+
+export function start() {
+    let a = document.querySelector("#b6");
+    a1.addEventListener("click", (event) => {
+        alert(ac.get());
+    });
+}
+
