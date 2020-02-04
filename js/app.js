4d4e5c24a5be76e18080de7d50aab69ed4e46b61
lucasburte <lucas.burte@hotmail.fr>
2020-02-04 11:45:06 +0100

    a commit

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-import ac from 'actions.js';
+import ac from './actions.js';
 
 export function start() {
     let a = document.querySelector("#b6");
-    a1.addEventListener("click", (event) => {
+    a.addEventListener("click", (event) => {
         alert(ac.get());
     });
 }
